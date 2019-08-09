89aafd21fce7c47c36b8fbe18d663ffd330acde8
Maciej Dzik <dzikdrums@gmail.com>
2019-08-09 10:43:46 +0200

    add article selector function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+  console.log('clickedElement (with plus): ' + clickedElement);
 
 
   /* [DONE] remove class 'active' from all article links  */
@@ -12,24 +12,30 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
   this.classList.add('active');
 
-
   /* [DONE] remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('article.active');
+  const activeArticles = document.querySelectorAll('article.active');
 
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
     }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  var articleSelector = clickedElement.getAttribute("href");
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
